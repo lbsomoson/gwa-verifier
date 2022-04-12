0996745948609e370a44c6a3ef746a273198d5ae
pcsuarez <pcsuarez@up.edu.ph>
2022-04-13 04:12:18 +0800

    Added signup function and changed login function to accomodate the users table specification

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,7 @@ const authController = require('./auth-controller');
 module.exports = (app) => {
 
    /* signUp is a limited function to admin users */
-  //app.post("/signup", authController.signUp);
+  app.get("/signup", authController.signUp);
 
   app.get("/login", authController.login);
   //app.get("/checkifloggedin", authController.checkIfLoggedIn);
