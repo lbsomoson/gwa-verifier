a7121f6b74d275ee1f0913d559cc0e0b13c0f65c
pcsuarez <pcsuarez@up.edu.ph>
2022-04-28 02:18:24 +0800

    Added updated files

--- backend/routes.js
+++ backend/routes.js
@@ -1,4 +1,7 @@
 const authController = require('./auth-controller');
+const controller = require('./controller');
+const myModule = require('./index');
+const {upload} = myModule.singleFile
 
 module.exports = (app) => {
 
@@ -6,5 +9,11 @@ module.exports = (app) => {
   app.get("/signup", authController.signUp);
 
   app.post("/loginUser", authController.login);
-  //app.get("/checkifloggedin", authController.checkIfLoggedIn);
+  app.post("/checkifloggedin", authController.checkIfLoggedIn);
+  app.post("/single", upload.array("files", 5), controller.uploadSingle)
+  app.get("/viewstudents", controller.findAllStudents)
+  app.get("/searchstudents", controller.searchStudents);
+  app.get("/sortstudents", controller.sortBy);
+  app.post("/deletestudent", controller.deleteStudent);
+  app.post("/deleteallstudents", controller.deleteAllStudents);
 }
