726befdf3e6a79da64ad5d21347530adba80cbb9
auenrique <auenrique@up.edu.ph>
2022-05-01 16:58:51 +0800

    conversion into functions

--- backend/create.js
+++ backend/create.js
@@ -45,35 +45,96 @@ ws['!ref'] = XLSX.utils.encode_range(range);
 
 var data = XLSX.utils.sheet_to_json(ws);
 
+verifyunits(data);
+checkload(data);
 //console.log(data);
 //convertpdf();
 // gets units of term to check for underloading and overloading
 //TODO use units to check underload and overload
-let calced = 0;
-for(let i = 0; i<data.length;i++){
-    if(data[i]["Term"]!=undefined){
-        recorded = data[i]["Term"];
-        if(recorded<15){
-            console.log(recorded, "Underload");
-        }else if(recorded>=15 && recorded<=21){
-            console.log(recorded, "Regular Load");
-        }else{
-            console.log(recorded, "Overload");
-        }
-        calced += data[i]["Units"];
-        if(recorded==calced){
-            console.log("Correct recorded units");
-        }else{
-            console.log("Incorrect recorded units");
-        }
-        calced = 0;
+function verifyname(fname,lname){
+    if(/^([A-Z\s])+$/.test(lname) && /^([A-Z\s])+$/.test(fname)){
+        console.log('Name: '+ lname+', '+fname);
     }else{
-        if(!isNaN(data[i]["Grade"])){
-            calced += data[i]["Units"];
-        }        
+        console.log(lname+', '+fname+' is not a valid name');   
     }
 }
 
+function verifystudno(studno){
+    if(/^20[0-2][0-9]-[0-9]{5}$/.test(studno)){
+        console.log('Student number: '+studno);
+    }else{
+        console.log('Invalid student number');
+    }
+}
+
+function verifycourse(course){
+    var coursecodes = ['BSCS','BACA'];
+    if(coursecodes.includes(course)){
+        console.log('Course: '+course);
+    }else{
+        console.log(course+ ' is not a valid course');
+    }
+}
+
+function verifyunits(data){
+    let calced = 0;
+    for(let i = 0; i<data.length;i++){
+        if(!isNaN(data[i]["Grade"])){
+            calced += data[i]["Units"];
+            if(!isNaN(data[i]["Term"])){
+                recorded = data[i]["Term"];
+                console.log("Calculated Units of Term:",calced)
+                console.log("Recorded Units:",recorded)
+                if(recorded==calced){
+                    console.log("Correct recorded units");
+                }else{
+                    console.log("Incorrect recorded units");
+                }
+                calced = 0;
+            }
+        }
+    }
+}
+
+function checkload(data){
+    for(let i = 0; i<data.length;i++){
+        if(data[i]["Term"]!=undefined){
+            recorded = data[i]["Term"];
+            if(recorded<15){
+                console.log(recorded, "Underload");
+            }else if(recorded>=15 && recorded<=21){
+                console.log(recorded, "Regular Load");
+            }else{
+                console.log(recorded, "Overload");
+            }
+        }
+    }
+}
+
+// let calced = 0;
+// for(let i = 0; i<data.length;i++){
+//     if(data[i]["Term"]!=undefined){
+//         recorded = data[i]["Term"];
+//         if(recorded<15){
+//             console.log(recorded, "Underload");
+//         }else if(recorded>=15 && recorded<=21){
+//             console.log(recorded, "Regular Load");
+//         }else{
+//             console.log(recorded, "Overload");
+//         }
+//         calced += data[i]["Units"];
+//         if(recorded==calced){
+//             console.log("Correct recorded units");
+//         }else{
+//             console.log("Incorrect recorded units");
+//         }
+//         calced = 0;
+//     }else{
+//         if(!isNaN(data[i]["Grade"])){
+//             calced += data[i]["Units"];
+//         }        
+//     }
+// }
 // checking content errors:
 // -check if valid student no, name, and course
 // -check if overloading/underloading
