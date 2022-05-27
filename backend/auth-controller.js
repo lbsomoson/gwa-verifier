1f872a8610bc899d5836e2bd0821c78038830b1a
pcsuarez <pcsuarez@up.edu.ph>
2022-05-28 03:51:32 +0800

    Sample for Refresh Tokens

--- backend/auth-controller.js
+++ backend/auth-controller.js
@@ -25,13 +25,14 @@ exports.login = (req, res) => {
                     username: result[0].Username,
                     type: result[0].Type
                 }
+                
+                const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {expiresIn: "5s"});
+                const refreshToken = jwt.sign(tokenPayload, process.env.JWT_SECRET2, {expiresIn: "1d"});
 
-                const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {expiresIn: "1d"});
-
-                return res.send({result, token, username})
+                return res.send({result, token, refreshToken, username})
             }
 
-        }//else
+        }
 
         return res.send({msg: "Not found"})
 
@@ -56,19 +57,53 @@ exports.signUp = (req, res) => {
 exports.checkIfLoggedIn = (req, res) => {
 
     if (!req.cookies || !req.cookies.authToken) {
+        console.log("No cookies");
         return res.send({ isLoggedIn: false });
     }
-    
+    console.log("Checking decoded . . . ");
     return jwt.verify(
         req.cookies.authToken,
         process.env.JWT_SECRET,
         (err, tokenPayload) => {
             if (err) {
-                return res.send({ isLoggedin: false });
-            }
+                console.log('Token is expired');
+                // since token is expired, check for refresh token
+                if(req.cookies.refreshToken) { // token exist
+                    // verify token
+                    jwt.verify(req.cookies.refreshToken, process.env.JWT_SECRET2, (err, refreshPayload) => {
+                        if(err) {
+                            return res.send({ isLoggedin: false});
+                        }
+
+                        const tokenPayload = {
+                            username: refreshPayload.username,
+                            type: refreshPayload.type
+                        }
+
+                        // sign new access token
+                        const newAccessToken = jwt.sign(tokenPayload, process.env.JWT_SECRET, {expiresIn: "5s"});
+
+                        //send cookie of new access token
+                        res.cookie("authToken", newAccessToken, 
+                            {
+                                path: "localhost:3001/",
+                                age: 86400,
+                                sameSite: "lax"
+                            }
+                        );
+
+                        return res.send({ isLoggedin: true, username: tokenPayload.username, type: tokenPayload.type }); 
+                    })
+                }
+                
+                return res.send({ isLoggedin: false});
+                
+            }   
 
             const user_name = tokenPayload.username;
+            console.log(user_name);
             const type = tokenPayload.type;
+            console.log(type);
 
             let findUser2 = 'SELECT * FROM users WHERE Username = ? and Type = ?';
 
@@ -78,12 +113,11 @@ exports.checkIfLoggedIn = (req, res) => {
                     console.log("err in db")
                     return res.send({ isLoggedin: false});
                 }
-                //console.log("Found user");
             })
 
-            //console.log("username: "+user_name)
             return res.send({ isLoggedin: true, username: user_name, type: type });
         });
+    
 }
 
 
