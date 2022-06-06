473dc811b73cf0d1cfd3f41e2e725486456175da
pcsuarez <pcsuarez@up.edu.ph>
2022-06-06 21:05:03 +0800

    Cleaned up code for verify_functions file and added try catch blocks to make sure code doesn't break

--- backend/verify_functions.js
+++ backend/verify_functions.js
@@ -5,12 +5,14 @@ function verifyname(filename, sheetName){
     //returns name if both first and last name are valid, returns an error if not
     var wb = XLSX.readFile("files/" + filename, {sheetStubs: true});
     var ws = wb.Sheets[sheetName];
+    
     //obtain name from expected location in sheet
-    var fname = ws['B1'].v;
-    var lname = ws['A1'].v;
-
-    console.log(fname)
-    console.log(lname)
+    try{
+        var fname = ws['B1'].v;
+        var lname = ws['A1'].v;
+    }catch{
+        return {"error": "Name is not valid"} 
+    }
 
     //regex for valid name
     if(/^([a-zA-Z])+$/.test(lname) && /^([a-zA-Z])+$/.test(fname) && (fname != undefined && lname !=undefined)){
@@ -24,23 +26,24 @@ function verifyname(filename, sheetName){
 function verifystudno(filename, sheetName){
     var wb = XLSX.readFile("files/" + filename, {sheetStubs: true}) ;
     var ws = wb.Sheets[sheetName];
+
     //obtain student number from expected location in sheet
-    var studno = String(ws['A2'].v);
-    console.log(studno);
-    //regex for valid student number
-    if(/^20[0-2][0-9]-[0-9]{5}$/.test(studno)){
-        //console.log('Student number: '+studno);
-    }else{
-        //if unsuccessful, check another expected location for student number and repeat process
-        studno = String(ws['A3'].v);
-        if(/^20[0-2][0-9]-[0-9]{5}$/.test(studno)){
-            //console.log('Student number: '+studno);
-        }else{
-            //console.log('Invalid student number');
-            return {"error": "Invalid student number"};
+    try{
+        var studno = String(ws['A2'].v);
+
+        //regex for valid student number
+        if(!/^20[0-2][0-9]-[0-9]{5}$/.test(studno)){
+            //if unsuccessful, check another expected location for student number and repeat process
+            studno = String(ws['A3'].v);
+            if(!/^20[0-2][0-9]-[0-9]{5}$/.test(studno)){
+                return {"error": "Invalid Student Number"};
+            }
         }
-        
+    }catch{
+        return {"error": "Invalid Student Number or Degree Program"};
     }
+
+    
     //returns student number in the form of a string
     return studno
 }
@@ -48,29 +51,29 @@ function verifystudno(filename, sheetName){
 function verifycourse(filename, sheetName){
     var wb = XLSX.readFile("files/" + filename, {sheetStubs: true});
     var ws = wb.Sheets[sheetName];
-    //obtain course from expected location in sheet
-    var course = String(ws['A3'].v);
-    console.log(course);
-    //check if course is included in config
-    if(config.programs.includes(course)){
-        //console.log('Course: '+course);
-    }else{
-        //if unsuccessful, check another expected location for course and repeat process
-        course = String(ws['A2'].v);
-        if(config.programs.includes(course)){
-            //console.log('Course: '+course);
-        }else{
-            //console.log(course+ ' is not a valid course');
-            return {"error": "Invalid course"};
+
+    // Obtain course from expected location in sheet
+    try{
+        var course = String(ws['A3'].v);
+
+        // Check if course is included in config
+        if(!config.programs.includes(course)){
+            // If unsuccessful, check another expected location for course and repeat process
+            course = String(ws['A2'].v);
+            if(!config.programs.includes(course)){
+                return {"error": "Invalid Degree Program"};
+            }
         }
-        
+    }catch{
+        return {"error": "Invalid Student Number or Degree Program"};
     }
+    
     //returns course in the form of a string
     return course;
 }
 
 function verifyHeaders(filename, sheetName){
-    //verifies correct format for headers
+    // Verifies correct format for headers
     var wb = XLSX.readFile("files/" + filename, {sheetRows: 4, sheetStubs: true});
     var ws = wb.Sheets[sheetName];
     var range = XLSX.utils.decode_range(ws['!ref']);
@@ -95,8 +98,7 @@ function verifyHeaders(filename, sheetName){
 
     for(let i=0; i<Object.keys(headers).length; i++){
         if(!(data[0][i] === headers[i])){
-            //if headers mismatch, prints expected header with obtained header and returns an error
-            console.log(`Expected ${headers[i]} but got ${data[0][i]}`)
+            // If headers mismatch, prints expected header with obtained header and returns an error
             return {'success': false, 'error':'Wrong format for headers'}
         }
     }
@@ -106,21 +108,13 @@ function verifyHeaders(filename, sheetName){
 }
 
 function verifyunits(data){
-    //compares calculated total units in term with total units recorded in sheet
-    //prints result in console
+    // Compares calculated total units in term with total units recorded in sheet
     let calced = 0;
     for(let i = 0; i<data.length;i++){
         if(!isNaN(data[i]["Grade"])){
             calced += data[i]["Units"];
             if(!isNaN(data[i]["Term"])){
                 recorded = data[i]["Term"];
-                console.log("Calculated Units of Term:",calced)
-                console.log("Recorded Units:",recorded)
-                if(recorded==calced){
-                    console.log("Correct recorded units");
-                }else{
-                    console.log("Incorrect recorded units");
-                }
                 calced = 0;
             }
         }
